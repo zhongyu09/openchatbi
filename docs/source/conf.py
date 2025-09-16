742407aa2121838149bd82ab60a39718da34e9e7
Yu Zhong <zhongyu8@gmail.com>
2025-09-16 10:58:04 +0800

    doc: add github Pages

---
+++ docs/source/conf.py
@@ -0,0 +1,87 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+project = 'OpenChatBI'
+copyright = '2025, Yu Zhong'
+author = 'Yu Zhong'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Mock dependencies for documentation build
+autodoc_mock_imports = ["langchain", "langchain_openai", "langchain_anthropic", "langgraph", "sqlalchemy", "chromadb"]
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "myst_parser",
+]
+
+# Set an environment variable to indicate we're building docs
+import os
+os.environ["SPHINX_BUILD"] = "1"
+
+# MyST parser configuration
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "html_admonition",
+    "html_image",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Autodoc configuration
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+# Napoleon configuration for Google/NumPy style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# GitHub Pages configuration
+html_baseurl = 'https://zhongyu09.github.io/openchatbi/'
+
+# Theme options for RTD theme
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'includehidden': True,
+    'titles_only': False
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,144 @@
+OpenChatBI Documentation
+========================
+
+.. include:: ../../README.md
+   :parser: myst_parser.sphinx_
+
+API Reference
+=============
+
+Core Module
+-----------
+
+.. automodule:: openchatbi
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Configuration
+-------------
+
+.. automodule:: openchatbi.config_loader
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Agent Graph
+-----------
+
+.. automodule:: openchatbi.agent_graph
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+State Management
+----------------
+
+.. automodule:: openchatbi.graph_state
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Utilities
+---------
+
+.. automodule:: openchatbi.utils
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Catalog System
+--------------
+
+.. automodule:: openchatbi.catalog
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: openchatbi.catalog.catalog_store
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: openchatbi.catalog.catalog_loader
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Text2SQL
+--------
+
+.. automodule:: openchatbi.text2sql
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: openchatbi.text2sql.generate_sql
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: openchatbi.text2sql.schema_linking
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Code Execution
+--------------
+
+.. automodule:: openchatbi.code
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: openchatbi.code.executor_base
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: openchatbi.code.local_executor
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+LLM Integration
+---------------
+
+.. automodule:: openchatbi.llm
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: openchatbi.llm.llm
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+Tools
+-----
+
+.. automodule:: openchatbi.tool
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: openchatbi.tool.run_python_code
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. automodule:: openchatbi.tool.ask_human
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`

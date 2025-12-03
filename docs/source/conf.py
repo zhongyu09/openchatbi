8cc5c916e64d4cbf6d27266144db2dc9e30e2bd2
Yu Zhong <zhongyu8@gmail.com>
2025-12-03 16:50:41 +0800

    release: v0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "OpenChatBI"
 copyright = "2025, Yu Zhong"
 author = "Yu Zhong"
-release = "0.2.0"
+release = "0.2.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
